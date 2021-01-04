6b6bba9a1572685eaff118a7c2675ddabf15f616
CindoddCindy <cindodcindy@gmail.com>
2021-01-04 10:34:27 +0700

    tambah folder ajah

---
+++ myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/MyonliestoretestApplication.java
@@ -0,0 +1,13 @@
+package mytokoonlinetest.myonliestoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyonliestoretestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyonliestoretestApplication.class, args);
+	}
+
+}
---
+++ myonliestoretest/src/test/java/mytokoonlinetest/myonliestoretest/MyonliestoretestApplicationTests.java
@@ -0,0 +1,13 @@
+package mytokoonlinetest.myonliestoretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyonliestoretestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
