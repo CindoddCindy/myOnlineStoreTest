51d2e94c72a5915db36fff0cd6f0fb84784c1789
CindoddCindy <cindodcindy@gmail.com>
2021-01-05 09:11:03 +0700

    tambah repo nya

--- myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/auth/repository/RoleRepository.java
+++ myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/auth/repository/RoleRepository.java
@@ -1,7 +1,17 @@
 package mytokoonlinetest.myonliestoretest.auth.repository;
 
 
-public interface RoleRepository  {
+import java.util.Optional;
+
+import mytokoonlinetest.myonliestoretest.auth.model.Role;
+import mytokoonlinetest.myonliestoretest.auth.model.RoleAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>  {
+    Optional<Role> findByName(RoleAccess roleName);
 
 
 }
--- myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/auth/repository/UserRepository.java
+++ myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/auth/repository/UserRepository.java
@@ -1,6 +1,16 @@
 package mytokoonlinetest.myonliestoretest.auth.repository;
 
+import java.util.Optional;
 
-public interface UserRepository  {
+import mytokoonlinetest.myonliestoretest.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>  {
+    Optional<User> findByName(String Name);
+    Boolean existsByName(String name);
+    Boolean existsByEmail(String email);
 
 }
