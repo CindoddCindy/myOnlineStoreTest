b4938328c87830aae736cd1f3675e86a98d02081
CindoddCindy <cindodcindy@gmail.com>
2021-01-13 13:54:14 +0700

    hapus kelas2 buy item

--- myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/main/repository/ItemRepositoryDua.java
+++
@@ -1,9 +0,0 @@
-package mytokoonlinetest.myonliestoretest.main.repository;
-
-import mytokoonlinetest.myonliestoretest.main.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepositoryDua extends JpaRepository<Item, Long> {
-}
