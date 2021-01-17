70dc795c99ebe91b595b337ffe15eab4eae170db
CindoddCindy <cindodcindy@gmail.com>
2021-01-17 13:56:16 +0700

    tambah set get dan constructor

--- myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/main/model/BuyItem.java
+++ myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/main/model/BuyItem.java
@@ -58,4 +58,107 @@ public class BuyItem extends AuditModel {
     @JsonIgnore
     private Item item;
 
+
+    public BuyItem(@NotNull String buyername, @NotNull String buyerphone, @NotNull String buyeraddress, @NotNull String itemname, @NotNull String itemimage, @NotNull String itemprice, @NotNull String itemaddress, @NotNull String itemqty, @NotNull String itemdesc) {
+        this.buyername = buyername;
+        this.buyerphone = buyerphone;
+        this.buyeraddress = buyeraddress;
+        this.itemname = itemname;
+        this.itemimage = itemimage;
+        this.itemprice = itemprice;
+        this.itemaddress = itemaddress;
+        this.itemqty = itemqty;
+        this.itemdesc = itemdesc;
+    }
+
+    public BuyItem() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBuyername() {
+        return buyername;
+    }
+
+    public void setBuyername(String buyername) {
+        this.buyername = buyername;
+    }
+
+    public String getBuyerphone() {
+        return buyerphone;
+    }
+
+    public void setBuyerphone(String buyerphone) {
+        this.buyerphone = buyerphone;
+    }
+
+    public String getBuyeraddress() {
+        return buyeraddress;
+    }
+
+    public void setBuyeraddress(String buyeraddress) {
+        this.buyeraddress = buyeraddress;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public String getItemimage() {
+        return itemimage;
+    }
+
+    public void setItemimage(String itemimage) {
+        this.itemimage = itemimage;
+    }
+
+    public String getItemprice() {
+        return itemprice;
+    }
+
+    public void setItemprice(String itemprice) {
+        this.itemprice = itemprice;
+    }
+
+    public String getItemaddress() {
+        return itemaddress;
+    }
+
+    public void setItemaddress(String itemaddress) {
+        this.itemaddress = itemaddress;
+    }
+
+    public String getItemqty() {
+        return itemqty;
+    }
+
+    public void setItemqty(String itemqty) {
+        this.itemqty = itemqty;
+    }
+
+    public String getItemdesc() {
+        return itemdesc;
+    }
+
+    public void setItemdesc(String itemdesc) {
+        this.itemdesc = itemdesc;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
 }
