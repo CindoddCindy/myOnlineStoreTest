26410719cbacd024199aba2bd15a5782d00d8116
CindoddCindy <cindodcindy@gmail.com>
2021-01-17 13:52:43 +0700

    tambah kelas buy item

--- myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/main/model/BuyItem.java
+++ myonliestoretest/src/main/java/mytokoonlinetest/myonliestoretest/main/model/BuyItem.java
@@ -1,4 +1,61 @@
 package mytokoonlinetest.myonliestoretest.main.model;
 
-public class BuyItem {
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import mytokoonlinetest.myonliestoretest.auth.model.User;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "buyitem")
+public class BuyItem extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String buyername;
+
+    @NotNull
+    private String buyerphone;
+
+    @NotNull String buyeraddress;
+
+    @NotNull
+    @Column(unique = true)
+    private String itemname;
+
+    @NotNull
+    @Column(unique = true)
+    private String itemimage;
+
+    @NotNull
+    @Column(unique = true)
+    private String itemprice;
+
+    @NotNull
+    @Column(unique = true)
+    private String itemaddress;
+
+    @NotNull
+    @Column(unique = true)
+    private String itemqty;
+
+    @NotNull
+
+    @Column(unique = true)
+    private String itemdesc;
+
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "item_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Item item;
+
 }
